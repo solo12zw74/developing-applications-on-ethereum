131196568105267f68700ce20404a74850073470
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-03-03 21:42:43 -0600

    Implement stub contract logic

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -2,6 +2,9 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract HelloWorld {
-  constructor() public {
+  uint public value;
+
+  function setValue(uint newValue) public {
+    value = newValue;
   }
 }
