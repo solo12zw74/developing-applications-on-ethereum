3f08d62931185252432160d9850b7eb396feaded
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-03-04 15:04:38 -0600

    Implement votes count limiting
    Create the mapping for storing those account, which already voted

--- contracts/Voter.sol
+++ contracts/Voter.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Voter {
   uint[] public votes;
   string[] public options;
+  mapping(address => bool) hasVoted;
 
   constructor(string[] memory _options) {
     options = _options;
@@ -12,6 +13,12 @@ contract Voter {
 
   function vote(uint option) public {
     require(option < options.length, "Invalid option");
+    require(!hasVoted[msg.sender], "Already voted");
+    recordVote(option);
+  }
+
+  function recordVote(uint option) private {
+    hasVoted[msg.sender] = true;
     votes[option] += 1;
   }
 
