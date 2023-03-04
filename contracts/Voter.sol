a53eb293ff1718fc76b13c1a20aaa31abe674b1f
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-03-04 16:35:50 -0600

    Implement voting based on mappings

--- contracts/Voter.sol
+++ contracts/Voter.sol
@@ -2,31 +2,57 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Voter {
-  uint[] public votes;
-  string[] public options;
+  struct Option {
+    uint votesCount;
+    bool exists;
+  }
+  struct VoteResult {
+    string option;
+    uint counter;
+  }
+  string[] private options;
+  uint private optionsLength;
+  mapping(string => Option) private optionsMap;
   mapping(address => bool) hasVoted;
-
+  
   constructor(string[] memory _options) {
+    require(_options.length < 32);
     options = _options;
-    votes = new uint[](_options.length);
+
+    optionsLength = 0;
+    for (uint index = 0; index < _options.length; index++) {
+      Option memory option = Option(0, true);
+      option.exists = true;
+      option.votesCount = 0;
+      optionsMap[_options[index]] = option;
+      optionsLength++;
+    }
   }
 
-  function vote(uint option) public {
-    require(option < options.length, "Invalid option");
+   function vote(string memory option) public {
     require(!hasVoted[msg.sender], "Already voted");
+    Option memory optionToCheck = optionsMap[option];
+    require(optionToCheck.exists, "Options for vote doesn't exists");
+
     recordVote(option);
   }
 
-  function recordVote(uint option) private {
+  function recordVote(string memory option) private {
     hasVoted[msg.sender] = true;
-    votes[option] += 1;
+    Option storage optionToVote = optionsMap[option];
+    optionToVote.votesCount += 1;
   }
 
   function getOptions() public view returns (string[] memory){
     return options;
   }
 
-  function getVotes() public view returns (uint[] memory){
-    return votes;
+  function getVotes() public view returns (VoteResult[] memory voteResult){
+      voteResult = new VoteResult[](optionsLength);
+      for (uint256 index = 0; index < optionsLength; index++) {
+        string memory optionName = options[index];
+        Option memory option = optionsMap[optionName];
+        voteResult[index] = VoteResult(optionName, option.votesCount);
+      }
   }
 }
