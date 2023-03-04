ca138f3f6a2620f2712abaa054706039fdd26945
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-03-04 16:41:26 -0600

    Update test to test the real behavior

--- test/hello_world.js
+++ test/hello_world.js
@@ -6,8 +6,11 @@ const HelloWorld = artifacts.require("HelloWorld");
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
 contract("HelloWorld", function (/* accounts */) {
-  it("should assert true", async function () {
-    await HelloWorld.deployed();
-    return assert.isTrue(true);
+  it("should return correct value", async function () {
+    let c = await HelloWorld.deployed();
+    let expectedValue = 11;
+    await c.setValue(expectedValue);
+    var actual = await c.value();
+    return assert.isTrue(actual == expectedValue);
   });
 });
